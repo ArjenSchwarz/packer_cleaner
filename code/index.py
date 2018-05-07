8e6f4d729a263b9a8d7c5a122bcde1edc4352a52
Arjen Schwarz <arjen.schwarz@bulletproof.net>
2018-05-07 21:18:52 +1000

    Add lots of debug information for finding issues

--- code/index.py
+++ code/index.py
@@ -15,6 +15,7 @@ client = boto3.client('ec2')
 
 
 def lambda_handler(event, context):
+    print("Starting the run")
     try:
         response = client.describe_instances(
             Filters=[
@@ -33,17 +34,27 @@ def lambda_handler(event, context):
             ]
         )
         instances_to_terminate = []
+        nr_instances_found = 0
         for reservation in response["Reservations"]:
+            print("Found a reservation")
             for instance in reservation["Instances"]:
+                print("Found an instance")
+                nr_instances_found += 1
                 launchTime = instance["LaunchTime"]
                 tz_info = launchTime.tzinfo
                 now = datetime.datetime.now(tz_info)
                 delta = datetime.timedelta(hours=max_runtime)
                 the_past = now - delta
+                print("Getting the calculated date")
+                print(the_past)
+                print("Getting the launchtime")
+                print(instance["LaunchTime"])
                 # If the instance was launched more than the max_runtime ago,
                 # get rid of it
                 if the_past > instance["LaunchTime"]:
                     instances_to_terminate.append(instance["InstanceId"])
+
+        print(str(nr_instances_found) + " matching instances found")
 
         if len(instances_to_terminate) > 0:
             print("These instances were running too long: ")
@@ -60,6 +71,8 @@ def lambda_handler(event, context):
             # Send an SNS message if the topic is defined
             if sns_topic != "":
                 send_sns(instances_to_terminate)
+        else:
+            print("No instances found that were running too long")
     except Exception as e:
         print(e)
         raise e
